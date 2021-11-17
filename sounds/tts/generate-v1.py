697566aaf95ca7cbd605f70e256798a40d5d7546
hjpaterson4 <91972322+hjpaterson4@users.noreply.github.com>
2021-11-17 11:32:51 -0500

    Update generate-v1.py

--- sounds/tts/generate-v1.py
+++ sounds/tts/generate-v1.py
@@ -7,26 +7,35 @@ FORMAT = "wav"
 LANG = "en"
 TLD = "co.uk"  # ["co.uk", "com", "ca", "co.in", "ie", "co.za"]
 
-
+room_ids= [
 def main():
-    msgs, paths = [], []
-    for i in range(N + 1):
-        if i == 0:
-            msgs.append("no points scored")
-        else:
-            msgs.append(f"{i} points scored")
-        paths.append(os.path.join(FOLDER, LANG, TLD, f"{i}.{FORMAT}"))
+    #marker_id is defined from find_door
+    for i in range(N+1):
+        if i == marker_id:
+            msgs.append(
+    
+    
+    
+    
+    
+   # msgs, paths = [], []
+   # for i in range(N + 1):
+     #   if i == 0:
+     #       msgs.append("no points scored")
+      #  else:
+      #      msgs.append(f"{i} points scored")
+      #  paths.append(os.path.join(FOLDER, LANG, TLD, f"{i}.{FORMAT}"))
 
-    msgs.append("nice job!")
-    paths.append(os.path.join(FOLDER, LANG, TLD, f"nice-job.{FORMAT}"))
+   # msgs.append("nice job!")
+   # paths.append(os.path.join(FOLDER, LANG, TLD, f"nice-job.{FORMAT}"))
 
-    msgs.append("new high score!")
-    paths.append(os.path.join(FOLDER, LANG, TLD, f"new-high-score.{FORMAT}"))
+   # msgs.append("new high score!")
+   # paths.append(os.path.join(FOLDER, LANG, TLD, f"new-high-score.{FORMAT}"))
 
-    for msg, path in zip(msgs, paths):
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        gTTS(msg, tld=TLD, lang=LANG).save(path)
+   # for msg, path in zip(msgs, paths):
+    #    os.makedirs(os.path.dirname(path), exist_ok=True)
+    #    gTTS(msg, tld=TLD, lang=LANG).save(path)
 
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
